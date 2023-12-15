3854de4553fc8f2eadfa714328f74b0baf937e1b
Ivan <zhmur.ivan@gmail.com>
2023-12-15 20:29:29 +0200

    Створив екземпляри інтерфейсу і протестував роботоспособність коду

--- Vechicle/HW/Program.cs
+++ Vechicle/HW/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HW;
+
+IVechicle vechicle = new Car();
+vechicle.Move();
+
+vechicle = new Truck();
+vechicle.Move();
+
+vechicle = new Ship();
+vechicle.Move();
